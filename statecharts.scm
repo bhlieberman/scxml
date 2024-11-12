540e202dbee6875d97d7a08b7f1c5c941b0745be
Ben Lieberman <bhlieberman93@gmail.com>
2024-11-11 19:53:25 -0700

    fix problem with hashing in guix (almost done)

--- statecharts.scm
+++ statecharts.scm
@@ -4,15 +4,14 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system dune))
 
-(define git-ref (git-reference (url "https://github.com/bhlieberman/scxml.git")
-			       (commit "53b6805")))
-
 (define-public statecharts
   (package
    (name "statecharts")
    (version "0.1")
-   (source (git-fetch git-ref 'sha256
-		      "04wkz7z3d9w9yy59d89abvmswsjdhksw270ia7a61pfdy8w97b2c"))
+   (source (origin (method git-fetch)
+		   (uri (git-reference (url "https://github.com/bhlieberman/scxml.git")
+				       (commit "c272052e648dfdf348ccda47ba0fb6ebd6aff5c9")))
+		   (hash (content-hash "0a7m951x0wj7rzrfdcsii5bb42s09z4db8wrfwjkg6linp0vyk8c"))))
    (build-system dune-build-system)
    (home-page "https://github.com/bhlieberman/scxml/")
    (synopsis "Implementation of the W3C Statechart XML notation in pure OCaml.")
